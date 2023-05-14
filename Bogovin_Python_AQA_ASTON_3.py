5ad994db194fb5d3ad1052cfadbd718463aa2d33
Vlad <116669526+aqaVlad@users.noreply.github.com>
2023-05-14 22:00:57 +0300

    UPD Test task

--- Bogovin_Python_AQA_ASTON_3.py
+++ Bogovin_Python_AQA_ASTON_3.py
@@ -1,13 +1,16 @@
 # 3. Составить алгоритм: на входе есть числовой массив,
 # необходимо вывести элементы массива кратные 3
 
-array = input("Введите числа через пробел:  ").split()
-L = [int(i) for i in array]
-for i in range(len(L)):
-    if L[i] % 3 == 0:
-        print(L[i])
+# array = input("Введите числа через пробел:  ").split()
+# L = [int(i) for i in array]
+# for i in range(len(L)):
+#     if L[i] % 3 == 0:
+#         print(L[i])
 
 
-array = list(map(int, input("Введите числа через пробел:  ").strip().split()))
-my_list = [i for i in array if i % 3 == 0]
-print(my_list)
+try:
+    array = list(map(int, input("Введите числа через пробел:  ").split()))
+    my_list = [i for i in array if i % 3 == 0]
+    print(my_list)
+except ValueError:
+    print("Это не число")
