ba6d8999aaf44606612197e996150203b98a186e
Vlad <116669526+aqaVlad@users.noreply.github.com>
2023-05-14 21:37:31 +0300

    UPD Test task

--- Bogovin_Python_AQA_ASTON_3.py
+++ Bogovin_Python_AQA_ASTON_3.py
@@ -6,3 +6,8 @@ L = [int(i) for i in array]
 for i in range(len(L)):
     if L[i] % 3 == 0:
         print(L[i])
+
+
+array = list(map(int, input("Введите числа через пробел:  ").strip().split()))
+my_list = [i for i in array if i % 3 == 0]
+print(my_list)
